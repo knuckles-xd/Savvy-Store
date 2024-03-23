40474b77b3b2acbbb3f6600b8d8d045d301774eb
zainabzaheer0 <163731961+zainabzaheer0@users.noreply.github.com>
2024-03-23 16:32:23 +0500

    Adding product model

--- SavvyStore/ecomm/app/models.py
+++ SavvyStore/ecomm/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+CategoryChoices=(
+    ('NA','New Arrival'),
+    ('PR','Pret'),
+    ('WS','Western'),
+    ('FR','Formals'),
+    ('AC','Accessories'),
+    ('FT','Footwear'),
+    ('FG','Fragrances'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    originalPrice = models.FloatField()
+    discountPrice = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    category = models.CharField(choices=CategoryChoices,max_length=2)
+    productImage = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+    
