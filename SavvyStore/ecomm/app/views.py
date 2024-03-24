0e45ea180166d887237ad82762f28503f39cf51a
zainabakram09 <163732811+zainabakram09@users.noreply.github.com>
2024-03-24 23:51:09 +0500

    Creating views for about and category pages

--- SavvyStore/ecomm/app/views.py
+++ SavvyStore/ecomm/app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from . models import Product
 
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+
+def about(request):
+    return render(request,'app/about.html',locals())
+
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title= Product.objects.filter(category=val).values('title')
+        return render(request,"app/category.html",locals())
+    
