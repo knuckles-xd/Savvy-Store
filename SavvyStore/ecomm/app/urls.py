4770ae5f3ddfb6ebeef1b92d28b1b729cac68a15
Muhammad Saad <151767995+knuckles-xd@users.noreply.github.com>
2024-03-25 00:15:43 +0500

    adding product detail url

--- SavvyStore/ecomm/app/urls.py
+++ SavvyStore/ecomm/app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('',views.home),
     path('about/',views.about,name="about"),
     path('category/<slug:val>',views.CategoryView.as_view(),name="category"),
+    path('category-title/<val>',views.CategoryTitle.as_view(),name="category-title"),
+    path('product-detail/<int:pk>',views.ProductDetail.as_view(),name="product-detail"),
      
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
