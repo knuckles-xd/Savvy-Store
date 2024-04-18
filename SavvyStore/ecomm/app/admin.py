6a895d1e4e663e6ef6546669384ae91ddcc60bf2
romaisayaqoob <129372767+romaisayaqoob@users.noreply.github.com>
2024-04-18 22:44:20 +0500

    Update admin.py adding customerModel

--- SavvyStore/ecomm/app/admin.py
+++ SavvyStore/ecomm/app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from . models import Product
+from . models import Product, Customer
 # Register your models here.
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
     list_display = ['id','title','discountPrice','category','productImage']
     search_fields = ['title']
-   
+@admin.register(Customer)
+class CutomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','name','address','city','state','zipcode']
