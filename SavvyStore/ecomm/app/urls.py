58caa5b48c8e9746e181be0d364bd301914bf1bb
Muhammad Saad <151767995+knuckles-xd@users.noreply.github.com>
2024-04-20 22:15:32 +0500

    Logout and Change Password Urls added

--- SavvyStore/ecomm/app/urls.py
+++ SavvyStore/ecomm/app/urls.py
@@ -3,7 +3,7 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm
+from .forms import LoginForm, MyPasswordChangeForm
 
 urlpatterns = [
     path('',views.home),
@@ -14,10 +14,17 @@ urlpatterns = [
     path('product-detail/<int:pk>',views.ProductDetail.as_view(),name="product-detail"),
     path('profile/',views.ProfileView.as_view(), name='profile'),
     path('address/',views.address, name='address'),
+    path('updateAddress/<int:pk>', views.updateAddress.as_view(), name="updateAddress"),
     
      #login authentication
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('accounts/login/',auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm, next_page = 'address'), name='login'),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
+    path('passwordchangedone/',auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'),name='passwordchangedone'),
+    path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'),
+    
+    
+    
     path('message/',views.message,name="message"),
      
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
