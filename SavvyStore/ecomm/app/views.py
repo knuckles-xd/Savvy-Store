277e7d810d6e95ceeab0276a913b1965c4462719
zainabakram09 <163732811+zainabakram09@users.noreply.github.com>
2024-04-26 15:17:27 +0500

    Adding cart fuctions

--- SavvyStore/ecomm/app/views.py
+++ SavvyStore/ecomm/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from . models import Product, Customer
+from . models import Product, Customer , Cart
 from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 
@@ -96,3 +96,21 @@ class updateAddress(View):
 
 def message(request):
     return render(request,'app/message.html',locals())
+
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    return redirect('/cart')
+
+
+def show_cart(request):
+    user = request.user
+    cart = Cart.objects.filter(user=user)
+    amount = 0
+    for i in cart:
+        amount += (i.product.discountPrice * i.quantity)
+    amount = round(amount,2)
+    totalamount = amount + 10
+    return render(request, 'app/addtocart.html',locals())
