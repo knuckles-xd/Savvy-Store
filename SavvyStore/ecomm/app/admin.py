a5d0536369b4962e50c558ce992019c5ab43651a
romaisayaqoob <129372767+romaisayaqoob@users.noreply.github.com>
2024-04-26 17:32:57 +0500

    Update admin.py added cart

--- SavvyStore/ecomm/app/admin.py
+++ SavvyStore/ecomm/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Product, Customer
+from . models import Product, Customer, Cart
 # Register your models here.
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
@@ -8,3 +8,6 @@ class ProductModelAdmin(admin.ModelAdmin):
 @admin.register(Customer)
 class CutomerModelAdmin(admin.ModelAdmin):
     list_display = ['id','user','name','address','city','state','zipcode']
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['id','product','quantity']
