82ddfabd7e01801de5b3fd723a3781d005951d32
romaisayaqoob <129372767+romaisayaqoob@users.noreply.github.com>
2024-04-29 00:27:31 +0500

    Update admin.py adding payment and orderplace

--- SavvyStore/ecomm/app/admin.py
+++ SavvyStore/ecomm/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Product, Customer, Cart
+from . models import Product, Customer, Cart, Payment, OrderPlaced
 # Register your models here.
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
@@ -11,3 +11,10 @@ class CutomerModelAdmin(admin.ModelAdmin):
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
     list_display = ['id','product','quantity']
+@admin.register(Payment)
+class PaymentModelAdmin(admin.ModelAdmin):
+    list_display = ['id','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','customer','product','quantity','ordered_date','status','payment']
+    
