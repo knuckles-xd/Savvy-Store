4d742c5568e5f10e7442fc0f1084c2760a71cb1a
romaisayaqoob <129372767+romaisayaqoob@users.noreply.github.com>
2024-05-01 04:06:10 +0500

    wishlist model added in admin site

--- SavvyStore/ecomm/app/admin.py
+++ SavvyStore/ecomm/app/admin.py
@@ -1,20 +1,32 @@
 from django.contrib import admin
-from . models import Product, Customer, Cart, Payment, OrderPlaced
+from . models import Product, Customer, Cart, Payment, OrderPlaced, Wishlist
+
 # Register your models here.
+
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
     list_display = ['id','title','discountPrice','category','productImage']
     search_fields = ['title']
+
+
 @admin.register(Customer)
 class CutomerModelAdmin(admin.ModelAdmin):
     list_display = ['id','user','name','address','city','state','zipcode']
+    
+
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
-    list_display = ['id','product','quantity']
+    list_display = ['id','user','product','quantity']
+    
 @admin.register(Payment)
 class PaymentModelAdmin(admin.ModelAdmin):
     list_display = ['id','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+    
+    
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
     list_display=['id','user','customer','product','quantity','ordered_date','status','payment']
-    
+
+@admin.register(Wishlist)
+class WishlistModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','product']
