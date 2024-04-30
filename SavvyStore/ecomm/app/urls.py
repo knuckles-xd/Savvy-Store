c02cf4b29ed58ad91a0acda94ff036ce335f0153
Muhammad Saad <151767995+knuckles-xd@users.noreply.github.com>
2024-04-30 18:35:25 +0500

    cart and payment URLs added

--- SavvyStore/ecomm/app/urls.py
+++ SavvyStore/ecomm/app/urls.py
@@ -15,6 +15,20 @@ urlpatterns = [
     path('profile/',views.ProfileView.as_view(), name='profile'),
     path('address/',views.address, name='address'),
     path('updateAddress/<int:pk>', views.updateAddress.as_view(), name="updateAddress"),
+    path('message/',views.message,name="message"),
+    
+    
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.show_cart, name='showcart'),
+    path('checkout/', views.checkout.as_view(), name='checkout'),
+    path('paymentdone/',views.payment_done,name='paymentdone'),
+    path('orders/', views.home, name='orders'),
+    
+    
+    path('pluscart/', views.plus_cart),
+    path('minuscart/', views.minus_cart),
+    path('removecart/', views.remove_cart),
+    
     
      #login authentication
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
@@ -27,6 +41,6 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html',form_class=MySetPasswordForm) , name='password_reset_confirm'),
     path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
     
-    path('message/',views.message,name="message"),
+    
      
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
